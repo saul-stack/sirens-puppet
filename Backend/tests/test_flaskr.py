ec68c615daa9d61813f1a4393c70f733992ac7a2
micatkinson <147393108+micatkinson@users.noreply.github.com>
2024-02-19 12:00:32 +0000

    add backend prompts for hangman and pictionary games

--- Backend/tests/test_flaskr.py
+++ Backend/tests/test_flaskr.py
@@ -7,3 +7,15 @@ def test_avatars():
     assert response.status_code == 200
     assert len(res) == 6
 
+def test_hangmanPrompts():
+    response = app.test_client().get('/hangmanPrompts')
+    res = json.loads(response.data.decode('utf-8')).get("HangmanPrompts")
+    assert response.status_code == 200
+    assert len(res) == 100
+
+def test_pictionaryPrompts():
+    response = app.test_client().get('/pictionaryPrompts')
+    res = json.loads(response.data.decode('utf-8')).get("PictionaryPrompts")
+    assert response.status_code == 200
+    assert len(res) == 47
+
