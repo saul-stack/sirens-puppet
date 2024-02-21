bd34aff7f183750c60a63fee57ae9247a395b058
darvatu <daniel.arvatu@gmail.com>
2024-02-21 14:36:03 +0000

    second page made with buttons

--- react-page/pirate-game/src/components/StoryPage.jsx
+++ react-page/pirate-game/src/components/StoryPage.jsx
@@ -1,12 +1,46 @@
-export default function StoryPage(){
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
+export default function StoryPage() {
+  let navigate = useNavigate();
+  const [username, setUsername] = useState("");
 
-    return (
-        <div className="parent">
-        <img src={"../../images/scroll.png"} className="story-scroll"/>
+  function handleJoin() {
+    navigate("/rooms");
+  }
+  function handleCreate() {
+    navigate("/newRoom");
+  }
+  function handleInput(value) {
+    setUsername(value);
+  }
+
+  return (
+    <>
+      <div className="parent">
+        <img src={"../../images/scroll.png"} className="story-scroll" />
         <div className="child">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus incidunt, numquam facilis odit cum nihil eum nulla, aliquam exercitationem error nisi fuga nobis possimus facere rerum, veniam sequi soluta deleniti.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+            incidunt, numquam facilis odit cum nihil eum nulla, aliquam
+            exercitationem error nisi fuga nobis possimus facere rerum, veniam
+            sequi soluta deleniti.
+          </p>
         </div>
       </div>
-    )
+      <form>
+        <label htmlFor="username">Enter Username</label>
+        <input
+          value={username}
+          onChange={(event) => handleInput(event.target.value)}
+          id="username"
+          type="text"
+          placeholder="Username"
+        />
+        <br />
+        <button onClick={handleJoin}>Join Room</button>
+        <button onClick={handleCreate}>Create Room</button>
+      </form>
+    </>
+  );
 }
