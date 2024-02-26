89a46957c5e27f9ade98c6a521e4d6b0a5f75e52
toyshe <toyshehossain@outlook.com>
2024-02-26 09:50:37 +0000

    Adding updated utils folder and chatbox component

--- react-page/pirate-game/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export function getAvatars(){
-    return axios.get("https://pirate-game-y566.onrender.com/avatars").then((response) => {
-        return response.data
-    })
-}
