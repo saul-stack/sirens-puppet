1f66deb1224fd509b2f24aebccca64ffc7154358
Saul <125879463+saul-stack@users.noreply.github.com>
2024-02-26 13:41:36 +0000

    remove extra arguments in socket emit statement

--- Frontend/src/components/Utils/Socket.js
+++ Frontend/src/components/Utils/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://pirate-game-react-nser.onrender.com"
+const URL = "https://pirate-game-react.onrender.com/";
 const socket = io(URL);
 
-export default socket
+export default socket;
