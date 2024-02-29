a139f40967d27631bc094d2faec9ace14b325939
Toyshe <95750580+toyshe@users.noreply.github.com>
2024-02-29 14:24:09 +0000

    Update GameRoom.jsx

--- Frontend/src/components/GameRoom.jsx
+++ Frontend/src/components/GameRoom.jsx
@@ -7,9 +7,8 @@ import ChatWindow from "./ChatWindow";
 import VotePage from "./VotePage";
 import { LivesContext } from "../contexts/LivesContext";
 import { useNavigate } from "react-router-dom";
-import Lives from "./Lives";
 
-function GameRoom(users, setUsers, playerList) {
+function GameRoom(users, setUsers) {
   const navigate = useNavigate;
   let teamLives = useContext(LivesContext);
   // console.log(teamLives.lives, "<----team Lives");
@@ -18,59 +17,66 @@ function GameRoom(users, setUsers, playerList) {
   const [showRoundPage, setShowRoundPage] = useState(false);
   const [showCanvasTestPage, setShowCanvasTestPage] = useState(false);
   const [gameOver, setGameOver] = useState(false);
-  const { user } = useContext(UserContext);
-  const { usersArray } = useContext(UserContext);
-  const [drawTurn, setDrawTurn] = useState(0);
-  const [guessTurn, setGuessTurn] = useState(1);
+  const { user, usersArray } = useContext(UserContext);
+  const [drawTurn, setDrawTurn] = useState(0)
+  const [guessTurn, setGuessTurn] = useState(1)
+  const [isDrawer, setIsDrawer] = useState()
+  const [isGuesser, setIsGuesser] = useState()
 
   const [round, setRound] = useState(0);
 
-  console.log(usersArray, "!!!!!useersArr!!!!!!");
+  console.log(users.users, '<users')
+  console.log(usersArray);
 
   // let drawTurn = -1;
   // let guessTurn = 0;
-  let currentDraw = "";
-  let currentGuess = "";
 
   const pickTurn = () => {
-    if (drawTurn > users.users.length) {
-      setDrawTurn(0);
+    setDrawTurn((prevTurn) => (prevTurn + 1))
+    setGuessTurn((prevTurn) => (prevTurn + 1))
+    console.log(drawTurn, '<<<<drawTurn');
+    console.log(guessTurn, '<<<<guessTurn');
+    if (drawTurn === users.users[0].length - 1) {
+      setDrawTurn(0)
     }
-
-    console.log(drawTurn, "<<drawTurn");
-    currentDraw = users.users[0][drawTurn];
-
-    currentDraw === user.username ? (user.draw = true) : (user.draw = false);
-    if (guessTurn > users.users.length) {
-      setGuessTurn(0);
+    const currentDraw = users.users[0][drawTurn]
+    setIsDrawer(currentDraw)
+    currentDraw === user.username ? user.draw = true : user.draw = false
+    console.log(currentDraw, '<<<<currentDaaw');
+    if (guessTurn === users.users[0].length - 1) {
+      setGuessTurn(0)
     }
-    console.log(guessTurn, "<<guessturn");
-    currentGuess = users.users[0][guessTurn];
-    currentGuess === user.username ? (user.guess = true) : (user.guess = false);
+    const currentGuess = users.users[0][guessTurn]
+    setIsGuesser(currentGuess)
+    console.log(currentGuess, '<<<<currentGuess');
+    currentGuess === user.username ? user.guess = true : user.guess = false
+    console.log(user.guess, '<<<<user.guess');
+    console.log(user.draw, '<<<<user.draw');
   };
 
-  console.log(user, "userUpdated ");
 
-  let playerDesignationLength = 5000;
-  let roundLength = 15000;
-  let roundBreakLength = 4000;
-
-  let numberOfRounds = 5;
-
+  console.log(user, 'userUpdated ');
+  
+  let playerDesignationLength = 3000;
+  let roundLength = 5000;
+  let roundBreakLength = 2000;
+  
+  let numberOfRounds = 3;
+  
   useEffect(() => {
     const playerDesignationTimer = setTimeout(() => {
       setShowPlayerDesignation(false);
       setShowRoundPage(true);
     }, playerDesignationLength);
-
+    
     return () => clearTimeout(playerDesignationTimer);
   }, []);
-
+  
   useEffect(() => {
     if (showRoundPage) {
-      setDrawTurn((prevTurn) => prevTurn + 1);
-      setGuessTurn((prevTurn) => prevTurn + 1);
-      pickTurn();
+      // drawTurn++;
+      // guessTurn++;
+      pickTurn()
       if (round + 1 > numberOfRounds) setGameOver(true);
       else {
         const roundPageTimer = setTimeout(() => {
@@ -95,35 +101,36 @@ function GameRoom(users, setUsers, playerList) {
   }, [showCanvasTestPage]);
 
   return (
-    <>
-      <div>
-        <Lives />
-        <h2>Lives: {teamLives.lives}</h2>
-        {teamLives.lives > 1
-          ? console.log("team has lives remaining")
-          : () => {
-              console.log("team has no lives remaining");
-              navigate("/endGamePageTest");
-            }}
-        {!gameOver && (
-          <div>
-            {showPlayerDesignation && <PlayerDesignation />}
-            {showRoundPage && <RoundPage round={round} setRound={setRound} />}
-            {showCanvasTestPage && (
-              <CanvasTestPage
-                timerCountdownSeconds={roundLength / 1000}
-                users={users}
-                setUsers={setUsers}
-                currentDraw={currentDraw}
-                currentGuess={currentGuess}
-              />
-            )}
-          </div>
-        )}
-        {gameOver && <VotePage playerList={users.playerList} />}
-        <ChatWindow />
-      </div>
-    </>
+    <div>
+      <h2>Lives: {teamLives.lives}</h2>
+      {teamLives.lives > 1
+        ? console.log("team has lives remaining")
+        : () => {
+          console.log("team has no lives remaining");
+          navigate("/endGamePageTest");
+        }}
+      {!gameOver && (
+        <div>
+          {showPlayerDesignation && <PlayerDesignation />}
+          {showRoundPage && <RoundPage round={round} setRound={setRound} />}
+          {showCanvasTestPage && (
+            <CanvasTestPage
+              timerCountdownSeconds={roundLength / 1000}
+              users={users}
+              setUsers={setUsers}
+              isDrawer={isDrawer}
+              isGuesser={isGuesser}
+            />
+          )}
+        </div>
+      )}
+      {gameOver && <VotePage playerList={users.playerList} />}
+      <ChatWindow />
+    </div>
+  );
+}
+
+export default GameRoom;
   );
 }
 
