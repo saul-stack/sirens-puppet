08bf57641e17198291a7078a43a987ea29d14638
Saul <125879463+saul-stack@users.noreply.github.com>
2024-03-07 16:45:32 +0000

    rename Siren's Thrall to Siren's Puppet

--- Frontend/src/components/PlayerDesignation.jsx
+++ Frontend/src/components/PlayerDesignation.jsx
@@ -1,30 +1,31 @@
-import { useNavigate } from "react-router-dom"
-import { UserContext } from "../contexts/UserContext"
-import { useContext } from "react"
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
+import { useContext } from "react";
 
-export default function PlayerDesignation(){
-    const { user } = useContext(UserContext)
-    return (
-        <>
-        <main>
-            <h1 className="player-title">You are...</h1>
-            <div className="parent">
-                <img src={"../../images/scroll2.png"} className="title-scroll"/>
-                    <div className="scroll-child">
-                        {user.isSaboteur && (
-                        <><h1>The Siren's Thrall</h1>
-                        <p> Sink the ship </p>
-                        </>)}
-                        {!user.isSaboteur && (
-                        <>
-                            <h1> Part of the crew</h1> 
-                            <p>Find the Thrall</p>
-                        </>
-                        )}
-                    </div>
-            </div>
-        </main>
-        </>
-    )
+export default function PlayerDesignation() {
+  const { user } = useContext(UserContext);
+  return (
+    <>
+      <main>
+        <h1 className="player-title">You are...</h1>
+        <div className="parent">
+          <img src={"../../images/scroll2.png"} className="title-scroll" />
+          <div className="scroll-child">
+            {user.isSaboteur && (
+              <>
+                <h1>The Siren's Puppet</h1>
+                <p> Sink the ship </p>
+              </>
+            )}
+            {!user.isSaboteur && (
+              <>
+                <h1> Part of the crew</h1>
+                <p>Find the Saboteur!</p>
+              </>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
-
--- Frontend/src/components/TitlePage.jsx
+++ Frontend/src/components/TitlePage.jsx
@@ -1,27 +1,27 @@
 import { useNavigate } from "react-router-dom";
 
-export default function TitlePage({isMute, playMusic, setMusicPlaying}) {
-  const navigate = useNavigate()
+export default function TitlePage({ isMute, playMusic, setMusicPlaying }) {
+  const navigate = useNavigate();
 
-  function handleClick(){
-    navigate("/story")
-    setMusicPlaying(true)
-    if (isMute){
-    playMusic()
+  function handleClick() {
+    navigate("/story");
+    setMusicPlaying(true);
+    if (isMute) {
+      playMusic();
     }
   }
 
   return (
     <>
-    <center>
-      <div className="parent">
-        <img src={"../../images/scroll2.png"} className="title-scroll"/>
-        <div className="scroll-child">
-          <h1>The Siren's Wrath</h1>
+      <center>
+        <div className="parent">
+          <img src={"../../images/scroll2.png"} className="title-scroll" />
+          <div className="scroll-child">
+            <h1>The Siren's Puppet</h1>
+          </div>
         </div>
-      </div>
-      <button onClick={handleClick}>Start Game</button>
-    </center>
+        <button onClick={handleClick}>Start Game</button>
+      </center>
     </>
   );
 }
